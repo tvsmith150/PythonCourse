d6a5a7118b8cc61230c23a053ac3e72e97b31173
Troy <tvsmith150@gmail.com>
2022-08-19 17:42:54 -0400

    Contact and movie added

---
+++ contacts.py
@@ -0,0 +1,16 @@
+import email
+
+
+contacts = {
+    "number":4,
+    "students":
+    [
+        {"name":"Sarah Holderness", "email": "sarah@example.com"},
+        {"name":"Harry Potter", "email": "harry@example.com"},
+        {"name":"Hermione Granger", "email":"hermione@example.com"},
+        {"name":"Ron Weasley", "email":"ron@example.com"}
+    ]
+}
+
+for student in contacts["students"]:
+    print(student["email"])
